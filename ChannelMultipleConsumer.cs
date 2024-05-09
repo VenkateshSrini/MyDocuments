563ebd759e9d0d368c3ba77be317ffe10eaf3b64
VenkateshSrini <venkateshsrini3@gmail.com>
2024-05-09 10:01:56 +0530

    Add files via upload

--- ChannelMultipleConsumer.cs
+++ ChannelMultipleConsumer.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Channels;
 using System.Threading.Tasks;
-namespace StringBuilderToMemStream;
+using Newtonsoft.Json;
+
 public class Producer
 {
     private readonly ChannelWriter<int> _writer;
@@ -27,50 +29,51 @@ public class Producer
 public class Consumer
 {
     private readonly ChannelReader<int> _reader;
+    private readonly ConcurrentBag<int> _bag;
 
-    public Consumer(ChannelReader<int> reader)
+    public Consumer(ChannelReader<int> reader, ConcurrentBag<int> bag)
     {
         _reader = reader;
+        _bag = bag;
     }
 
     public async Task ConsumeAsync()
     {
-        while (await _reader.WaitToReadAsync())
+        await foreach (var item in _reader.ReadAllAsync())
         {
-            if (_reader.TryRead(out var item))
+            if (await ProcessItem(item))
             {
-                // Call your synchronous method here
-                await Task.Run(() => ProcessItem(item));
+                _bag.Add(item);
             }
         }
-
-        //await foreach (var item in _reader.ReadAllAsync())
-        //{
-        //    await Task.Run(() => ProcessItem(item));
-        //}
     }
 
-    private void ProcessItem(int item)
+    private async Task<bool> ProcessItem(int item)
     {
         // Simulate processing time
-        System.Threading.Thread.Sleep(100);
+        await Task.Delay(100);
         Console.WriteLine($"Processed {item} on thread {Task.CurrentId}");
+        return true; // return your actual result here
     }
 }
 
 class EntryPoint
 {
+
     public void Run()
     {
         var channel = Channel.CreateUnbounded<int>();
+        var bag = new ConcurrentBag<int>();
 
         var producer = new Producer(channel.Writer);
-        var consumers = Enumerable.Range(0, 10).Select(_ => new Consumer(channel.Reader)).ToList();
+        var consumers = Enumerable.Range(0, 10).Select(_ => new Consumer(channel.Reader, bag)).ToList();
 
         var producerTask = producer.ProduceAsync();
         var consumerTasks = consumers.Select(c => c.ConsumeAsync()).ToArray();
 
         Task.WaitAll(consumerTasks.Concat(new[] { producerTask }).ToArray());
+
+        var json = JsonConvert.SerializeObject(bag);
+        Console.WriteLine(json);
     }
-    
 }
